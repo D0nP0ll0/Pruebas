1ddebe33a5251aaeb1ad69c1308e29810117d812
Dolvem <contact@dolvem.com>
2024-03-16 23:52:02 +0100

    Variación E05: Reutilizar código con función y reintentos

--- E05_RevillaV.java
+++ E05_RevillaV.java
@@ -5,15 +5,15 @@
  */
 
 /**
- *
  * @author alber
  */
 //Importem la biblioteca per introduïr dades.
+
 import java.util.Scanner;
 
 public class E05_RevillaV {
 
-    //Declarem les puntuacions com a constants
+    // Declarem les puntuacions com a constants
     public static final int PUNTS_GERMA = 40;
     public static final int PUNTS_POBL = 30;
     public static final int PUNTS_FEIN = 20;
@@ -22,92 +22,39 @@ public class E05_RevillaV {
     public static final int PUNTS_HIST = 5;
 
     public static void main(String[] args) {
-        //Declarem les variables a introduir
+        // Declarem les variables a introduir
         int num, germa, pobl, fein, dismal, fam, hist, punts = 0;
 
-        //Engeguem el lector
+        // Engeguem el lector
         Scanner lector = new Scanner(System.in);
 
-        //Presa de dates
-        //mostrar: Numero Alumne;
-        //numnen = llegir sencer 
-        System.out.print("Introdueix Numero de registre del nen:");
-        //Verifiquem que el valor esta entre 1 i 499 a mes de ser un sencer
-        if (lector.hasNextInt()) {
-            num = lector.nextInt();
-            if (num >= 1 && num <= 499) {
-            } else {
-                System.out.println("Numero de registre erroni");
-                return; //He trobat aquesta opcio ja que sino continua executant el codi
-            }
-        } else {
-            System.out.println("Numero de registre erroni");
-            return;
-        }
-        //mostrar: Te germans a l'escola? (0: no  /  1:si);
-        //germa = llegir sencer
-        System.out.print("Te germans a l'escola? (0:no / 1:si)");
-        germa = lector.nextInt();
-        if (germa == 0 || germa == 1) { //Assegurem que no s'ha introduit altre valor
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //mostrar: Viu a la poblacio?  (0: no  /  1:si);
-        //resideix = llegir sencer
-        System.out.print("Resideix a la poblacio? (0:no / 1:si)");
-        pobl = lector.nextInt();
-         if (pobl == 0 || pobl == 1) {
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //mostrar: Treballen els pares a la poblacio? (0: no  /  1:si);
-        //feina = llegir sencer
-        System.out.print("Treballen els pares a la poblacio (0:no / 1:si)");
-        fein = lector.nextInt();
-         if (fein == 0 || fein == 1) {
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //mostrar: Te alguna discapacitat o malaltia cronica? (0: no  /  1:si);
-        //discaMalal = llegir sencer
-        System.out.print("Te alguna discapacitat o malaltia cronica? (0:no / 1:si)");
-        dismal = lector.nextInt();
-         if (dismal == 0 || dismal == 1) {
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //mostrar: Familia nombrosa o monoparental? (0: no  /  1:si);
-        //fam1_3 = llegir sencer
-        System.out.print("Familia monoparental o numerosa? (0:no / 1:si)");
-        fam = lector.nextInt();
-         if (fam == 0 || fam == 1) {
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //mostrar: Va estudiar algun pare o germa a l'escola? (0: no  /  1:si);
-        //historial = llegir sencer
-        System.out.print("Han estudiat els pares o germans al centre? (0:no / 1:si)");
-        hist = lector.nextInt();
-         if (hist == 0 || hist == 1) {
-        } else {
-            System.out.println("Si us plau, introdueix 0 per 'no' o 1 per 'si'.");
-            return;
-        }
-        //Calcul de la puntuacio
-        
-        //si germa = 1;
+        // Presa de dates
+
+        // mostrar: Numero Alumne;
+        num = demanarNumero("Introdueix Nº de registre del nen: ", 1, 499, lector);
+        // mostrar: Te germans a l'escola? (0: no  /  1:si);
+        germa = demanarNumero("¿Té germans a l'escola? (0:no / 1:si): ", 0, 1, lector);
+        // mostrar: Viu a la poblacio?  (0: no  /  1:si);
+        pobl = demanarNumero("¿Resideix a la poblacio? (0:no / 1:si): ", 0, 1, lector);
+        // mostrar: Treballen els pares a la poblacio? (0: no  /  1:si);
+        fein = demanarNumero("¿Treballen els pares a la poblacio? (0:no / 1:si): ", 0, 1, lector);
+        // mostrar: Te alguna discapacitat o malaltia cronica? (0: no  /  1:si);
+        dismal = demanarNumero("¿Té alguna discapacitat o malaltia cronica? (0:no / 1:si): ", 0, 1, lector);
+        // mostrar: Familia nombrosa o monoparental? (0: no  /  1:si);
+        fam = demanarNumero("¿Familia monoparental o numerosa? (0:no / 1:si): ", 0, 1, lector);
+        // mostrar: Va estudiar algun pare o germa a l'escola? (0: no  /  1:si);
+        hist = demanarNumero("¿Han estudiat els pares o germans al centre? (0:no / 1:si): ", 0, 1, lector);
+
+        // Calcul de la puntuacio
+
+        // si germa = 1;
         //	punts = + 40;
         if (germa == 1) {
             punts = punts + PUNTS_GERMA;
         }
-        //si resideix = 1;
+        // si resideix = 1;
         //	punts = + 30;
-        //si no,
+        // si no,
         //	si feina = 1;
         //		punts = + 20;
         if (pobl == 1) {
@@ -117,24 +64,44 @@ public class E05_RevillaV {
                 punts = punts + PUNTS_FEIN;
             }
         }
-        //si discaMalal = 1;
+        // si discaMalal = 1;
         //	punts = + 10;
         if (dismal == 1) {
             punts = punts + PUNTS_DISMAL;
         }
-        //si fam1_3 = 1;
+        // si fam1_3 = 1;
         //	punts = + 15;
         if (fam == 1) {
             punts = punts + PUNTS_FAM;
         }
-        //si historial = 1;
+        // si historial = 1;
         //	punts = + 5
         if (hist == 1) {
             punts = punts + PUNTS_HIST;
         }
-        //Mostra puntuacio
-        //mostrar: Puntuacio alumne:;
-        //mostrar: "Alumne" + numnen + punts + "/100"
-        System.out.println("Alumne N:" + num + "\tPunts:" + punts);
+
+        // Mostra puntuacio
+
+        // mostrar: Puntuacio alumne:;
+        // mostrar: "Alumne" + numnen + punts + "/100"
+        System.out.println("\nAlumne Nº: " + num + "\tPunts: " + punts + "/100");
+    }
+
+    /**
+     * Demana un valor a l'usuari.
+     *
+     * @return el valor introduit per l'usuari
+     */
+    private static int demanarNumero(String missatge, int min, int max, Scanner lector) {
+        System.out.print(missatge);
+        int resultat = lector.nextInt();
+        while (resultat < min || resultat > max) {
+            System.out.println("Si us plau, introduiex un valor entre " + min + " i " + max + ".");
+            System.out.print(missatge);
+            if (lector.hasNextInt()) {
+                resultat = lector.nextInt();
+            }
+        }
+        return resultat;
     }
 }
